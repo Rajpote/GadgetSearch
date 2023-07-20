54a8131d07a800f45c13054aeb1bca84dc5565ff
Rajpote <rrajpote@gmail.com>
2023-07-20 22:22:02 +0545

    forn validated

--- register.php
+++ register.php
@@ -28,9 +28,13 @@ if (isset($_POST['submit'])) {
             $password = password_hash($password, PASSWORD_DEFAULT);
 
             // Insert the user data into the database
-            $sql = "INSERT INTO register (uname, email, password, cpassword, gender, phnumber, address) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $sql = "INSERT INTO register (uname, email, password, gender, phnumber, address) VALUES (?, ?, ?, ?, ?, ?)";
             $stmt = $pdo->prepare($sql);
-            $stmt->execute([$uname, $email, $password, $cpassword, $gender, $phnumber, $address]);
+            $stmt->execute([$uname, $email, $password, $gender, $phnumber, $address]);
+
+            // Set success variable
+            $success = 1;
+
             echo '<script> alert("User registered successfully."); window.location.href = "home.php"; </script>';
         }
     }
@@ -60,7 +64,6 @@ if (isset($_POST['submit'])) {
 
     <link rel="stylesheet" href="style2.css">
     <title>Register</title>
-
 </head>
 
 <body>
@@ -72,14 +75,29 @@ if (isset($_POST['submit'])) {
             <div class="container">
                 <label for="uname">User name:</label>
                 <input type="text" id="uname" name="uname" required />
+                <div id="name-error" class="error-message"></div>
             </div>
             <div class="container">
-                <label for="password">Password:</label>
-                <input type="password" id="pass" name="password" required />
+                <!-- <span class="toggle-password" id="toggle-pass"> -->
+                    <label for="password">Password</label>
+                    <input type="password" id="pass" name="password" placeholder="Password" />
+                    <span class="eye" onclick="togglePassword()">
+                        <i id="hideopen" class="fa-solid fa-eye" style="color: #849a9a;"></i>
+                        <i id="hideclose" class="fa-solid fa-eye-slash" style="color: #849a9a;"></i>
+                    </span>
+                <!-- </span> -->
+                <div id="password-error" class="error-message"></div>
             </div>
             <div class="container">
-                <label for="C password">Confirm password:</label>
-                <input type="password" id="cpass" name="cpassword" required />
+                <!-- <span class="toggle-password" id="toggle-cpass"> -->
+                    <label for="cpassword">Confirm Password</label>
+                    <input type="password" id="cpass" name="cpassword" placeholder="Confirm Password" />
+                    <span class="eye" onclick="toggleCPassword()">
+                        <i id="Chideopen" class="fa-solid fa-eye" style="color: #849a9a;"></i>
+                        <i id="Chideclose" class="fa-solid fa-eye-slash" style="color: #849a9a;"></i>
+                    </span>
+                <!-- </span> -->
+                <div id="confirm-password-error" class="error-message"></div>
             </div>
             <div class="container">
                 <label for="gender">Gender:</label>
@@ -91,12 +109,14 @@ if (isset($_POST['submit'])) {
                 </select><br>
             </div>
             <div class="container">
-                <label for="phone number">Phone Number:</label>
+                <label for="phnumber">Phone Number:</label>
                 <input type="number" id="phnumber" name="phnumber" required />
+                <div id="phnumber-error" class="error-message"></div>
             </div>
             <div class="container">
                 <label for="email">E-mail:</label>
                 <input type="email" id="email" name="email" required />
+                <div id="email-error" class="error-message"></div>
             </div>
             <div class="container">
                 <label for="Address">Address:</label>
@@ -104,16 +124,39 @@ if (isset($_POST['submit'])) {
             </div>
             <div class="container0">
                 <input type="checkbox">
-                <span>I agree with all thr <strong>term & condition</strong> </span>
+                <span>I agree with all the <strong>terms & conditions</strong></span>
             </div>
             <div class="reg-btn">
                 <input type="submit" class="submit" name="submit" id="submit" value="Register">
             </div>
         </form>
         <script src="validate_signup.js"> </script>
-
     </div>
-</body>
 
+    <?php
+    if (isset($success) && $success == 1) {
+        ?>
+        <script>
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 1500,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener("mouseenter", Swal.stopTimer)
+                    toast.addEventListener("mouseleave", Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: "success",
+                title: "Signup Successfully"
+            });
+        </script>
+        <?php
+    }
+    ?>
+</body>
 
 </html>
